e778e7b1d33ba7c67a21dd1a0c27eff3c1e49cbc
Daniil <d.dulov98@gmail.com>
2021-03-21 14:09:25 +0400

    Homework for the 3rd Geekbrains class

---
+++ GB_HW_Class 3/EX4.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+extern const int a = 10;
+extern const int b = 34;
+extern const int c = 27;
+extern const int d = 83;
+
+void mainEX4()
+{
+
+}
---
+++ GB_HW_Class 3/GB_HW_Class 3 CPP.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+using namespace std;
+
+// Ex. 1; 4
+
+extern const int a;
+extern const int b;
+extern const int c;
+extern const int d;
+int main()
+{
+    float e = a * (b + (static_cast <float> (c) / d));
+
+    cout << e << endl;
+
+    // Ex. 2
+
+    int NUM, RESULT;
+
+    cout << "Enter the number: ";
+    cin >> NUM;
+    RESULT = (NUM <= 21) ? 21 - NUM : (NUM - 21) << 1;
+
+    cout << RESULT << endl;
+
+    // Ex. 3
+
+    int array[3][3][3], *ptr;
+    ptr = &array[0][0][0];
+    ptr += 13;
+    *ptr = 3;
+    cout << array[1][1][1];
+
+
+}
